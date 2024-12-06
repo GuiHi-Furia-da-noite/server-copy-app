e7d38b4639f703fb82619cecd30db1e24a0cfcb5
guilhermeteixeira <guilherme.teixeira@fieldpro.com.br>
2024-12-06 12:04:27 -0300

    Faz o L

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
+import http from 'http';
 import app from './app';
 
+const server = http.createServer(app);
+
 const PORT = process.env.PORT || 3001;
+
+server.timeout = 5 * 60 * 1000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
